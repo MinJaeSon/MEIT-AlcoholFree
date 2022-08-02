9f7a9f702a6bae4dd5e0ce9d9674525724aa86fd
MinJaeSon <mjsonsj@gmail.com>
2022-08-02 16:55:16 +0900

    FEAT: 액티비티 전환

--- app/src/main/java/com/example/meit_alcoholfree/NowMeasureActivity.java
+++
@@ -1,2 +0,0 @@
-package com.example.meit_alcoholfree;public class NowMeasureActivity {
-}
--- app/src/main/java/com/example/meit_alcoholfree/StartMeasureActivity.java
+++ app/src/main/java/com/example/meit_alcoholfree/StartMeasureActivity.java
@@ -7,6 +7,7 @@ import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
@@ -27,129 +28,34 @@ import app.akexorcist.bluetotohspp.library.DeviceList;
 
 public class StartMeasureActivity extends AppCompatActivity {
     ImageView startBtn;
-    private BluetoothSPP bt;
+    TextView startTv;
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_measure_start);
-        bt = new BluetoothSPP(this);
-        startBtn = (ImageView)findViewById(R.id.measure_start_iv);
 
-        if (!bt.isBluetoothAvailable()) { //블루투스 사용 불가
-//            Toast.makeText(getApplicationContext()
-//                    , "Bluetooth is not available"
-//                    , Toast.LENGTH_SHORT).show();
-            Log.i("BT", "Bluetooth is not available");
-            finish();
-        }
-
-        bt.setOnDataReceivedListener(new BluetoothSPP.OnDataReceivedListener() { //데이터 수신
-            public void onDataReceived(byte[] data, String message) {
-//                Toast.makeText(StartMeasureActivity.this, message, Toast.LENGTH_SHORT).show();
-                Log.i("BTData", message);
-            }
-        });
-
-        bt.setBluetoothConnectionListener(new BluetoothSPP.BluetoothConnectionListener() { //연결됐을 때
-            public void onDeviceConnected(String name, String address) {
-//                Toast.makeText(getApplicationContext()
-//                        , "Connected to " + name + "\n" + address
-//                        , Toast.LENGTH_SHORT).show();
-                Log.i("BTConnect", "Connected to " + name + "\n" + address);
-            }
-
-            public void onDeviceDisconnected() { //연결해제
-//                Toast.makeText(getApplicationContext()
-//                        , "Connection lost", Toast.LENGTH_SHORT).show();
-                Log.i("BTConnect", "Connection lost");
-            }
-
-            public void onDeviceConnectionFailed() { //연결실패
-//                Toast.makeText(getApplicationContext()
-//                        , "Unable to connect", Toast.LENGTH_SHORT).show();
-                Log.i("BTConnect", "Unable to connect");
-            }
-        });
+        startBtn = (ImageView) findViewById(R.id.measure_start_iv);
+        startTv = (TextView) findViewById(R.id.measure_start_tv);
 
         startBtn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                if (bt.getServiceState() == BluetoothState.STATE_CONNECTED) {
-                    bt.disconnect();
-                } else {
-                    Intent intent = new Intent(getApplicationContext(), DeviceList.class);
-                    startActivityForResult(intent, BluetoothState.REQUEST_CONNECT_DEVICE);
-                }
+            @Override
+            public void onClick(View view) {
+                startTv.setText("측정 중...");
+                measured();
             }
         });
     }
 
-    // 앱 중단시 (액티비티 나가거나, 특정 사유로 중단시)
-    public void onDestroy() {
-        super.onDestroy();
-        bt.stopService(); //블루투스 중지
-    }
-
-    // 앱이 시작하면
-    public void onStart() {
-        super.onStart();
-        if (!bt.isBluetoothEnabled()) {
-            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
-            startActivityForResult(intent, BluetoothState.REQUEST_ENABLE_BT);
-        } else {
-            if (!bt.isServiceAvailable()) {
-                bt.setupService();
-                bt.startService(BluetoothState.DEVICE_OTHER); //DEVICE_ANDROID는 안드로이드 기기 끼리
-                setup();
-            }
-        }
-    }
-
-    // 블루투스 사용 - 데이터 전송
-    public void setup() {
-        startBtn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                bt.send("1", true);
-                changeTv();
-            }
-        });
-    }
-
-    // 음주측정 시작 후 진행중 표시
-    private void changeTv() {
-        TextView startTv = (TextView) findViewById(R.id.measure_start_tv);
-        startTv.setText("측정 중...");
-    }
-
-    // 새로운 액티비티에 반환
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == BluetoothState.REQUEST_CONNECT_DEVICE) {
-            if (resultCode == Activity.RESULT_OK)
-                bt.connect(data);
-        } else if (requestCode == BluetoothState.REQUEST_ENABLE_BT) {
-            if (resultCode == Activity.RESULT_OK) {
-                bt.setupService();
-                bt.startService(BluetoothState.DEVICE_OTHER);
-                setup();
-            } else {
-//                Toast.makeText(getApplicationContext()
-//                        , "Bluetooth was not enabled."
-//                        , Toast.LENGTH_SHORT).show();
-                Log.i("BT", "Bluetooth was not enabled.");
+    public void measured() {
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(StartMeasureActivity.this, CompleteMeasureActivity.class);
+                startActivity(intent);
                 finish();
             }
-        }
+        }, 10000);
     }
 
 }
